773be2a421d7acb4280e1fc4b244cf117cdfd6b6
hlemonnier <hugo.lemonnier01@gmail.com>
2023-10-24 21:14:17 +0200

    separation graph crypto stocks

--- main.py
+++ main.py
@@ -23,18 +23,8 @@ def get_crypto_data():
         crypto_data.append({"Name": name, "Symbol": symbol, "Market Cap": market_cap})
 
     crypto_df = pd.DataFrame(crypto_data)
-    print("Before modification:")
-    print(crypto_df.head())
-    
     crypto_df['Symbol'] = crypto_df['Symbol'].apply(lambda x: x + '-USD')
-    
-    print("\nAfter modification:")
-    print(crypto_df.head())
-
     crypto_df.to_excel("cryptos_market_cap.xlsx", index=False)
-
-# Lisez le fichier Excel contenant les cryptos
-crypto_data = pd.read_excel("cryptos_market_cap.xlsx")
 
 def get_data(tickers, start_date, end_date):
     data = yf.download(tickers, start=start_date, end=end_date)['Adj Close']
@@ -44,7 +34,7 @@ def calculate_returns(data):
     returns = data.pct_change().dropna()
     return returns
 
-def efficient_frontier(returns):
+def efficient_frontier(returns,target_return_):
     cov_matrix = returns.cov()
     avg_returns = returns.mean()
 
@@ -52,7 +42,7 @@ def efficient_frontier(returns):
     args = (avg_returns, cov_matrix)
 
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1},
-                   {'type': 'eq', 'fun': lambda x: np.sum(x * avg_returns) - target_return})
+                   {'type': 'eq', 'fun': lambda x: np.sum(x * avg_returns) - target_return_})
 
     bounds = tuple((0, 1) for asset in range(num_assets))
     results = minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args, constraints=constraints, bounds=bounds)
@@ -61,60 +51,101 @@ def efficient_frontier(returns):
 def portfolio_volatility(weights, avg_returns, cov_matrix):
     return np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
 
-# Récupérer les données des cryptomonnaies
 get_crypto_data()
-
-# Lisez le fichier Excel contenant les cryptos
 crypto_data = pd.read_excel("cryptos_market_cap.xlsx")
-
-# Obtenez la liste des symboles des cryptos
 crypto_symbols = crypto_data['Symbol'].tolist()
 
-# Récupérer les données des actions
-# Assuming the tickers are in a column named 'Ticker' in the all_tickers.xlsx file
 all_stocks = pd.read_excel("all_tickers.xlsx")
 first_100_stocks = all_stocks['Symbol'][:10].tolist()
 
-tickers = []
-tickers.extend(first_100_stocks)
-tickers.extend(crypto_symbols)  # Ajout des symboles des cryptos à la liste tickers
+crypto_data = get_data(crypto_symbols, '2015-01-01', '2023-10-01')
+stocks_data = get_data(first_100_stocks, '2015-01-01', '2023-10-01')
 
-data = get_data(tickers, '2015-01-01', '2023-10-01')
+crypto_daily_returns = calculate_returns(crypto_data)
+stocks_daily_returns = calculate_returns(stocks_data)
 
-# Calculer les rendements quotidiens
-daily_returns = calculate_returns(data)
+crypto_avg_daily_returns = crypto_daily_returns.mean()
+stocks_avg_daily_returns = stocks_daily_returns.mean()
 
-# Calculez le rendement moyen et l'écart type pour chaque action
-avg_daily_returns = daily_returns.mean()
-risks = daily_returns.std()
+crypto_risks = crypto_daily_returns.std()
+stocks_risks = stocks_daily_returns.std()
 
-# Créez un DataFrame pour stocker le rendement moyen et le risque de chaque action
-results = pd.DataFrame({
-    "Rendement moyen": avg_daily_returns,
-    "Risque": risks
-})
 
-# Sauvegardez ces résultats dans un fichier Excel
-results.to_excel("rendements_et_risques.xlsx")
+# # Créez un DataFrame pour stocker le rendement moyen et le risque de chaque action
+# Create dictionary of results
+results_dict = {
+    "Stocks": pd.DataFrame({
+        "Rendement moyen": stocks_avg_daily_returns,
+        "Risque": stocks_risks
+    }),
+    "Crypto": pd.DataFrame({
+        "Rendement moyen": crypto_avg_daily_returns,
+        "Risque": crypto_risks
+    })
+}
 
-# Pour différentes cibles de rendement, définir la target_return et trouver les poids optimaux
-target_returns = np.linspace(daily_returns.min().min(), daily_returns.max().max(), 100)
-efficient_portfolios = []
-for target_return in target_returns:
-    efficient_portfolios.append(efficient_frontier(daily_returns))
+# Use ExcelWriter to save these DataFrames into separate sheets of the same Excel file
+with pd.ExcelWriter("rendements_et_risques.xlsx") as writer:
+    for sheet_name, result in results_dict.items():
+        result.to_excel(writer, sheet_name=sheet_name)
+#
+# # Pour différentes cibles de rendement, définir la target_return et trouver les poids optimaux
+target_returns_stock = np.linspace(stocks_avg_daily_returns.min(), stocks_avg_daily_returns.max(), 100)
+target_returns_crypto = np.linspace(crypto_avg_daily_returns.min(), crypto_avg_daily_returns.max(), 100)
 
-portfolio_volatilities = [portfolio['fun'] for portfolio in efficient_portfolios]
+# Dictionary containing average daily returns and daily returns for each asset type
+data_dict = {
+    "Stocks": (stocks_avg_daily_returns, stocks_daily_returns),
+    "Crypto": (crypto_avg_daily_returns, crypto_daily_returns)
+}
 
-# Existing plotting code for efficient frontier
-plt.plot(portfolio_volatilities, target_returns, 'y-', label='Frontière Efficient de Markowitz')
+# Empty dictionary to store efficient portfolios for each asset type
+efficient_portfolios_dict = {}
 
-# New code to plot individual asset points without names
-for i, ticker in enumerate(tickers):
-    plt.scatter(risks[ticker], avg_daily_returns[ticker], marker='o', s=20)  # s=20 sets a smaller size for the scatter points
+# Loop over the dictionary
+for asset_type, (avg_daily_returns, daily_returns) in data_dict.items():
+    target_returns = np.linspace(avg_daily_returns.min(), avg_daily_returns.max(), 100)
+    efficient_portfolios = [efficient_frontier(daily_returns, target_return) for target_return in target_returns]
+    efficient_portfolios_dict[asset_type] = efficient_portfolios
 
-plt.xlabel('Volatilité (Écart type du rendement)')
-plt.ylabel('Rendement attendu')
-plt.legend()
-plt.title('Frontière Efficient de Markowitz')
-plt.grid(True)  # This will add gridlines for better readability
-plt.show()
+# Dictionary containing data for plotting
+plotting_data = {
+    "Cryptos": {
+        "risks": crypto_risks,
+        "avg_daily_returns": crypto_avg_daily_returns,
+        "symbols": crypto_symbols,
+        "efficient_portfolios": efficient_portfolios_dict["Crypto"],
+        "color": 'b',
+        "title": 'Cryptos: Rendement vs Risque'
+    },
+    "Stocks": {
+        "risks": stocks_risks,
+        "avg_daily_returns": stocks_avg_daily_returns,
+        "symbols": first_100_stocks,
+        "efficient_portfolios": efficient_portfolios_dict["Stocks"],
+        "color": 'r',
+        "title": 'Stocks: Rendement vs Risque'
+    }
+}
+
+# Loop over the dictionary to plot for each asset type
+for key, data in plotting_data.items():
+    plt.figure(figsize=(10, 6))
+
+    # Plotting the efficient frontier
+    portfolio_volatilities = [portfolio['fun'] for portfolio in data["efficient_portfolios"]]
+    target_returns = np.linspace(data["avg_daily_returns"].min(), data["avg_daily_returns"].max(), 100)
+    plt.plot(portfolio_volatilities, target_returns, 'y-', label='Frontière Efficient de Markowitz')
+
+    # Plotting individual asset points with their names
+    plt.scatter(data["risks"], data["avg_daily_returns"], marker='o', s=20, color=data["color"])
+    for ticker in data["symbols"]:
+        plt.annotate(ticker, (data["risks"][ticker], data["avg_daily_returns"][ticker]), fontsize=8, alpha=0.7)
+
+    plt.xlabel('Volatilité (Écart type du rendement)')
+    plt.ylabel('Rendement attendu')
+    plt.title(data["title"])
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
