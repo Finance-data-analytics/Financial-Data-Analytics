13ff2817d4c8a4b1c007493fddab6f9778446f5c
hlemonnier <hugo.lemonnier01@gmail.com>
2023-11-20 14:20:53 +0100

    login required for survey

--- neoma/__init__.py
+++ neoma/__init__.py
@@ -9,6 +9,6 @@ app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = "login_page"
+login_manager.login_view = "login_register_page"
 login_manager.login_message_category = "info"
 from neoma import routes
