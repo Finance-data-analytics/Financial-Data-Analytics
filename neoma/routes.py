ac339683b088cf335e5f3c18432cc9fba01ea744
hlemonnier <hugo.lemonnier01@gmail.com>
2023-11-21 01:37:16 +0100

    commit new

--- neoma/routes.py
+++ neoma/routes.py
@@ -5,12 +5,15 @@ from neoma.models import *
 from neoma.forms import *
 from neoma import db
 from flask_login import *
+
 from portfolio_analysis import best_weigth
+
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('index.html')
+
 
 @app.route('/login_register', methods=['GET', 'POST'])
 def login_register_page():
@@ -44,11 +47,13 @@ def login_register_page():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('login.html', login_form=login_form, register_form=register_form)
 
+
 @app.route('/logout')
 def logout_page():
     logout_user()
     flash("You have been logged out!", category='info')
     return redirect(url_for("home_page"))
+
 
 @app.route('/combined_survey_investment', methods=['GET', 'POST'])
 @login_required
@@ -87,47 +92,63 @@ def combined_survey_investment():
             flash('Please correct the errors in the form.', 'danger')
     return render_template('build_portfolio.html', risk_form=risk_form, investment_form=investment_form)
 
+
 @app.route('/select_portfolio', methods=['GET', 'POST'])
 @login_required
 def portfolio_options():
     PortfolioSelection = PortfolioSelectionForm()
-    if 'portfolio_bool' not in session:
+    if 'top_5_portfolios' in session:
+        plot_data = generate_plotly_data(session['top_5_portfolios'])
+        session.pop('top_5_portfolios', None)
+    else:
         crypto_weight_limit, stocks_data, crypto_data, capital, Top_5_Selection = recommend_and_display_portfolio(
             session['portfolio_type'], session['capital'], session['investment_horizon'], session['nb_stocks'])
         top_5_transformed = transform_top_5_selection(Top_5_Selection)
         plot_data = generate_plotly_data(top_5_transformed)
-    if PortfolioSelection.validate_on_submit():
-        session['portfolio_bool'] = True
-        selected_portfolio_number = int(PortfolioSelection.portfolio_choice.data)
-        selected_portfolio = top_5_transformed[selected_portfolio_number]
-        # Extract stocks and cryptos from the selected portfolio
-        selected_stocks = selected_portfolio.get('stocks', [])
-        selected_cryptos = selected_portfolio.get('cryptos', [])
-        (combined_selected_assets, monetary_allocation, best_weights, ret_arr_allocation, vol_arr_allocation
-         , sharpe_arr_allocation) = best_weigth(crypto_weight_limit, stocks_data, crypto_data, capital, selected_stocks,
-                                                selected_cryptos)
-        plot_data = generate_optimal_weight_plot_data(vol_arr_allocation, ret_arr_allocation, sharpe_arr_allocation)
-        max_sharpe_idx = sharpe_arr_allocation.argmax()
-        data_portfolio = [ret_arr_allocation[max_sharpe_idx], vol_arr_allocation[max_sharpe_idx]
-            , sharpe_arr_allocation[max_sharpe_idx]]
-        list_weight_selected_assets_json = json.dumps(best_weights.tolist())
-        data_portfolio_json = json.dumps([arr.tolist() for arr in data_portfolio])
 
-        new_portfolio = Portfolio(
-            user_id=current_user.id,
-            name='User Portfolio',
-            list_selected_assets=json.dumps(selected_stocks + selected_cryptos),
-            list_weight_selected_assets=list_weight_selected_assets_json,
-            data_portfolio=data_portfolio_json,
-            is_invested=False,
-            capital=capital,
-            horizon=session['investment_horizon'],
-        )
-        db.session.add(new_portfolio)
-        db.session.commit()
-        session.pop('portfolio_bool', None)
-        return render_template('plot_choosen_portfolio.html', plot_data=plot_data,data=data_portfolio)
+        if PortfolioSelection.validate_on_submit():
+            session['top_5_portfolios'] = top_5_transformed
+            session['portfolio_bool'] = True
+            selected_portfolio_number = int(PortfolioSelection.portfolio_choice.data)
+            selected_portfolio = top_5_transformed[selected_portfolio_number]
+            # Extract stocks and cryptos from the selected portfolio
+            selected_stocks = selected_portfolio.get('stocks', [])
+            selected_cryptos = selected_portfolio.get('cryptos', [])
+            (combined_selected_assets, monetary_allocation, best_weights, ret_arr_allocation, vol_arr_allocation
+             , sharpe_arr_allocation) = best_weigth(crypto_weight_limit, stocks_data, crypto_data, capital,
+                                                    selected_stocks,
+                                                    selected_cryptos)
+            plot_data = generate_optimal_weight_plot_data(vol_arr_allocation, ret_arr_allocation, sharpe_arr_allocation)
+            max_sharpe_idx = sharpe_arr_allocation.argmax()
+            data_portfolio = [ret_arr_allocation[max_sharpe_idx], vol_arr_allocation[max_sharpe_idx]
+                , sharpe_arr_allocation[max_sharpe_idx]]
+            list_weight_selected_assets_json = json.dumps(best_weights.tolist())
+
+            capital_allocation = [weight * capital for weight in best_weights]
+            # Combine the assets, weights, and capital into a single list for the template
+            assets_info = [
+                f"{asset} ({weight * 100:.1f}% - {cap:.2f}€)"
+                for asset, weight, cap in zip(combined_selected_assets, best_weights, capital_allocation)
+            ]
+            future_value = capital * ((1 + ret_arr_allocation[max_sharpe_idx]) ** session['investment_horizon'])
+            new_portfolio = Portfolio(
+                user_id=current_user.id,
+                name='User Portfolio',
+                list_selected_assets=json.dumps(selected_stocks + selected_cryptos),
+                list_weight_selected_assets=list_weight_selected_assets_json,
+                data_portfolio=json.dumps(data_portfolio),
+                is_invested=False,
+                capital=capital,
+                horizon=session['investment_horizon'],
+            )
+            db.session.add(new_portfolio)
+            db.session.commit()
+            session.pop('portfolio_bool', None)
+
+            return render_template('plot_choosen_portfolio.html', plot_data=plot_data, data=data_portfolio,
+                                   fv=round(future_value, 3),portfolio_details=assets_info)
     return render_template('plot_portfolios.html', plot_data=plot_data, form=PortfolioSelection)
+
 
 @app.route('/my_portfolio', methods=['GET', 'POST'])
 @login_required  # Assuming you have a login_required decorator to protect this route
